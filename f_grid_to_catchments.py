518554170e3c4eeda3dbb0f0cab2cf76ada02e1d
Fransje van Oorschot <vanoorschot@tintin.bo.isac.cnr.it>
2022-03-02 17:30:15 +0100

    test2

---
+++ f_grid_to_catchments.py
@@ -0,0 +1,215 @@
+import glob
+from pathlib import Path
+
+import geopandas as gpd
+import iris
+import iris.pandas
+import numpy as np
+from esmvalcore import preprocessor
+from iris.coords import DimCoord
+from iris.cube import Cube
+from pathos.threading import ThreadPool as Pool
+from datetime import datetime
+from datetime import timedelta
+
+
+def regridding_target_cube(catchment_shapefile, spatial_resolution, buffer=1):
+    catchment_bounds = gpd.read_file(catchment_shapefile).bounds
+
+    buffer = 1
+    minx = int(catchment_bounds.minx.values[0]) - buffer
+    maxx = int(catchment_bounds.maxx.values[0]) + buffer
+    miny = int(catchment_bounds.miny.values[0]) - buffer
+    maxy = int(catchment_bounds.maxy.values[0]) + buffer
+
+    latitude = DimCoord(
+        np.linspace(
+            miny,
+            maxy,
+            int(np.divide(abs(miny-maxy), spatial_resolution)),
+            dtype=float,
+        ),
+        standard_name="latitude",
+        units="degrees",
+    )
+    latitude.guess_bounds()
+    
+    longitude = DimCoord(
+        np.linspace(
+            minx,
+            maxx,
+            int(np.divide(abs(minx-maxx), spatial_resolution)),
+            dtype=float,
+        ),
+        standard_name="longitude",
+        units="degrees",
+    )
+    longitude.guess_bounds()
+    
+    target_cube = Cube(
+        np.zeros((len(latitude.points), len(longitude.points)), np.float32),
+        dim_coords_and_dims=[(latitude, 0), (longitude, 1)],
+    )
+
+    return target_cube
+
+
+def area_weighted_shapefile_rasterstats(
+    catchment_shapefile,
+    catchment_netcdf,
+    statistical_operator,
+    output_dir,
+    output_csv=True,
+    return_cube=False,
+    regrid_first=True,
+    grid_resolution=0.1
+):
+    
+    """
+    Calculate area weighted zonal statistics of netcdfs using a shapefile to extract netcdf data.
+
+    catchment_shapefile:  str, catchment shapefile
+    catchment_netcdf:     str, netcdf file
+    statistical_operator: str, (mean, median (NOT area weighted), sum, variance, min, max, rms)
+    - https://docs.esmvaltool.org/projects/esmvalcore/en/latest/api/esmvalcore.preprocessor.html#esmvalcore.preprocessor.area_statistics
+    output_csv:          bool, True stores csv output and False stores netcdf output
+    regrid_first:        bool, True regrid cube first before extracting shape, False do not regrid first
+    grid_resolution:    float, grid cell size of target cube in degrees
+    Returns: iris cube, stores .csv file or .nc file
+    """
+    
+    # Load iris cube of netcdf
+    cube = iris.load_cube(catchment_netcdf)
+    cube.dim_coords[1].guess_bounds()
+    cube.dim_coords[2].guess_bounds()
+    
+    # extract dates for filename
+    time_start,time_end = cube.coord('time')[0],cube.coord('time')[-1]
+    point_start, point_end = time_start.points, time_end.points
+    unit = time_start.units
+    l = unit.num2date(0)
+    d = datetime(year=l.year, month=l.month, day=l.day)
+    date_start, date_end = d + timedelta(days=point_start[0]), d + timedelta(days=point_end[0])
+    y_start, y_end = date_start.year, date_end.year
+    
+    # Create target grid and regrid cube
+    if regrid_first is True:
+        target_cube = regridding_target_cube(
+            catchment_shapefile, grid_resolution, buffer=1
+        )
+  
+        cube = preprocessor.regrid(cube, target_cube, scheme="area_weighted")
+
+    # From cube extract shapefile shape
+    cube = preprocessor.extract_shape(
+        cube, catchment_shapefile, method="contains"
+    )
+
+    # Calculate area weighted statistics
+    cube_stats = preprocessor.area_statistics(cube, statistical_operator)
+
+    if output_csv is True:
+        # Convert cube to dataframe
+        df = iris.pandas.as_data_frame(cube_stats)
+
+        # Change column names
+        df = df.reset_index()
+        df = df.set_axis(["time", cube_stats.name()], axis=1)
+        
+        var=0
+        if (catchment_netcdf.split('/')[-1].split('_')[0] == 'pr'):
+            var='P'
+        if (catchment_netcdf.split('/')[-1].split('_')[0] == 'Ep'):
+            var='Ep'
+        if (catchment_netcdf.split('/')[-1].split('_')[0] == 'tas'):
+            var='T'
+
+        # Write csv as output
+        df.to_csv(
+            f"{output_dir}/{Path(catchment_shapefile).name.split('.')[0]}_{var}_{statistical_operator}_{y_start}_{y_end}.csv"
+        )
+    else:
+        iris.save(
+            cube_stats,
+            f"{output_dir}/{Path(catchment_shapefile).name.split('.')[0]}_{var}_{statistical_operator}_{y_start}_{y_end}.nc",
+        )
+    #     # Write csv as output
+    #     df.to_csv(
+    #         f"{output_dir}/{Path(catchment_shapefile).name.split('.')[0]}_{catchment_netcdf.split('/')[-1].split('_')[0]}_{statistical_operator}_{y_start}_{y_end}.csv"
+    #     )
+    # else:
+    #     iris.save(
+    #         cube_stats,
+    #         f"{output_dir}/{Path(catchment_shapefile).name.split('.')[0]}_{catchment_netcdf.split('/')[-1].split('_')[0]}_{statistical_operator}_{y_start}_{y_end}.nc",
+    #     )
+
+    if return_cube == True:
+        return cube
+    else:
+        return
+    
+
+def run_function_parallel(
+    shapefile_list=list,
+    netcdf_list=list,
+    operator_list=list,
+    output_dir_list=list,
+    threads=None
+    #threads=100
+):
+    """
+    Runs function area_weighted_shapefile_rasterstats in parallel.
+
+    shapefile_list:  str, list, list of input catchment shapefiles
+    netcdf_list:     str, list, list of input netcdf files
+    operator_list:   str, list, list of statistical operators (single operator)
+    output_dir_list: str, list, list of output directories
+    threads:         int,       number of threads (cores), when set to None use all available threads
+
+    Returns: None
+    """
+    # Set number of threads (cores) used for parallel run and map threads
+    if threads is None:
+        pool = Pool()
+    else:
+        pool = Pool(nodes=threads)
+    # Run parallel models
+    results = pool.map(
+        area_weighted_shapefile_rasterstats,
+        shapefile_list,
+        netcdf_list,
+        operator_list,
+        output_dir_list,
+    )
+
+    return results
+
+def construct_lists_for_parallel_function(NC4_DIR, SHAPE_DIR, OUT_DIR):
+    """
+    This functions constructs list for running code in parallel.
+
+    NC4_DIR:              str, dir containing input netcdf files for area weighted statitic calculations
+    SHAPE_DIR:            str, dir containing catchment shapefiles
+    OUT_DIR:              str, dir for output storage
+
+    Returns: list of netcdfs, shapefiles, output directories
+    """
+    netcdfs = glob.glob(f"{NC4_DIR}/*nc")
+    shapefiles = glob.glob(f"{SHAPE_DIR}/*shp")
+
+    output_dir = [OUT_DIR]
+
+    shapefile_list = shapefiles * len(netcdfs)
+    shapefile_list.sort()
+    netcdf_list = netcdfs * len(shapefiles)
+    output_dir_list = output_dir * len(shapefile_list)
+
+    operator_list = []
+
+    for netcdf in netcdf_list:
+        if "tas" in netcdf:
+            operator_list.append("mean")
+        else:
+            operator_list.append("mean")
+
+    return shapefile_list, netcdf_list, operator_list, output_dir_list
---
+++ f_preprocess_discharge.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import glob
+
+def preprocess_gsim_discharge(catch_id, fol_in, fol_out):
+    catch_id_c = catch_id.upper()
+    filepath = glob.glob(f'{fol_in}/{catch_id_c}*')[0]
+
+    b = pd.read_csv(filepath,delimiter=':',skiprows=10,nrows=7, index_col=0)
+    c = pd.DataFrame(index=[0],columns=['gsim.no','river','station','country','lat_deg','lon_deg','alt_m','area_km2'])
+    c['gsim.no'] = str.strip(b.columns[0])
+    c['river'] = str.strip(b.iloc[0,0])
+    c['station'] = str.strip(b.iloc[1,0])
+    c['country'] = str.strip(b.iloc[2,0])
+    c['lat_deg'] = str.strip(b.iloc[3,0])
+    c['lon_deg'] = str.strip(b.iloc[4,0])
+    c['alt_m'] = str.strip(b.iloc[5,0])
+    c['area_km2'] = str.strip(b.iloc[6,0])
+    c = c.replace('',np.nan)
+    c[['lat_deg','lon_deg','alt_m','area_km2']] = c[['lat_deg','lon_deg','alt_m','area_km2']].astype(float)
+
+
+    # save catchment timeseries in dataframe
+    a = pd.read_csv(filepath, skiprows=21, delimiter=',',index_col=0)
+    a.index = pd.to_datetime(a.index)
+    a.columns = a.columns.str.strip() # remove \t (=tab))    
+    # drop columns we don't need
+    a = a.drop(columns=['"GINI"','"CV"','"CT"','"P10"', '"P20"', '"P30"', '"P40"', '"P50"', '"P60"', '"P70"', '"P80"', '"P90"'])
+    a = a.astype(str)
+
+    # remove all tabs
+    for i in range(len(a)): #loop over rows
+        for j in range(len(a.iloc[0])): # loop over columns
+            a.iloc[i,j] = a.iloc[i,j].strip()
+
+    # replace NA with 999
+    a = a.replace('NA',np.nan)
+
+    #rename columns
+    a = a.rename(columns={'"MEAN"': 'mean_m3s', '"SD"': 'sd_m3s', '"IQR"': 'iqr_m3s', '"MIN"': 'min_m3s', '"MAX"': 'max_m3s',
+            '"MIN7"': 'min7_m3s', '"MAX7"': 'max7_m3s', '"DOYMIN"':'doymin','"DOYMAX"':'doymax', '"DOYMIN7"':'doy7min', '"DOYMAX7"':'doy7max',
+            '"n.missing"':'nr_mis_days','"n.available"':'nr_av_days'})
+    a = a.astype(float)
+
+    #convert values to mm/day
+    area_m2 = c.area_km2.values[0] * (10**6)
+    a_mm = a
+    a_mm = a_mm.rename(columns={'mean_m3s':'mean_mmd', 'sd_m3s':'sd_mmd', 'iqr_m3s':'iqr_mmd', 'min_m3s':'min_mmd', 'max_m3s':'max_mmd', 'min7_m3s':'min7_mmd','max7_m3s':'max7_mmd'})
+    a_mm[['mean_mmd', 'sd_mmd', 'iqr_mmd', 'min_mmd', 'max_mmd', 'min7_mmd','max7_mmd']] = a[['mean_m3s', 'sd_m3s', 'iqr_m3s', 'min_m3s', 'max_m3s', 'min7_m3s','max7_m3s']] * (1/area_m2) * 1000 * 86400
+    a_mm = a_mm.rename(columns={'mean_m3s':'mean_mmd', 'sd_m3s':'sd_mmd', 'iqr_m3s':'iqr_mmd', 'min_m3s':'min_mmd', 'max_m3s':'max_mmd', 'min7_m3s':'min7_mmd','max7_m3s':'max7_mmd'})
+    a_mm = a_mm.astype(float)
+    a_mm = a_mm.rename(columns={'mean_mmd':'Q'})
+
+    # add length timeseries to c
+    c['start_year'] = a_mm.index[0]
+    c['end_year'] = a_mm.index[-1]
+    c['length_ts_yr'] = int((a_mm.index[0] - a_mm.index[-1]).days / -365)
+    c['start_year'] = pd.to_datetime(c['start_year'])
+    c['end_year'] = pd.to_datetime(c['end_year'])
+
+    # save files 
+    c.to_csv(f'{fol_out}/characteristics/{catch_id}.csv')
+    # a.to_csv(f'{fol_out}/timeseries/{catch_id}.csv')
+    a_mm.to_csv(f'{fol_out}/timeseries/{catch_id}.csv')
+    
+    return a_mm
+    
+    
